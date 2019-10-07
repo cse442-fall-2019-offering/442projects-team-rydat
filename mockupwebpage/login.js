c372ef85d75a0bb7c276004c54aab36d5a285c4e
RHowe22 <robjhowe22@gmail.com>
2019-10-06 23:24:24 -0400

    #40 able to connectto spotify authorization page using example code

--- mockupwebpage/login.js
+++ mockupwebpage/login.js
@@ -1,12 +1,13 @@
 // this link works
 // https://accounts.spotify.com/authorize?client_id=c00c658f3a8a4a29961ae9fe70ffde7e&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123
 // do this function uses http://jsfiddle.net/JMPerez/62wafrm7/ for reference
+// https://developer.spotify.com/documentation/web-api/libraries/
 
 function dothis() {
     
     function login(callback) {
         var CLIENT_ID = 'c00c658f3a8a4a29961ae9fe70ffde7e';
-        var REDIRECT_URI = 'http://localhost:8888/callback';
+        var REDIRECT_URI = 'http://localhost:5500/callback';
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
@@ -46,39 +47,16 @@ function dothis() {
         });
     }
 
-    var templateSource = document.getElementById('result-template').innerHTML,
-        template = Handlebars.compile(templateSource),
-        resultsPlaceholder = document.getElementById('result'),
-        loginButton = document.getElementById('btn');
+    var loginButton = document.getElementById('btn');
     
-    loginButton.addEventListener('click', function() {
+
         login(function(accessToken) {
             getUserData(accessToken)
                 .then(function(response) {
-                    loginButton.style.display = 'none';
-                    resultsPlaceholder.innerHTML = template(response);
+                    console.log("hello");
+                    window.location.href ="index.html"
                 });
             });
-    });
+  
     
-}
-function test(){
-    window.addEventListener("message", function(event) {
-        var hash = JSON.parse(event.data);
-        if (hash.type == 'access_token') {
-            callback(hash.access_token);
-            document.getElementById("trythis").innerHTML=hash.accessToken;
-        }
-    }, false);
-
-    var width = 450,
-    height = 730,
-    left = (screen.width / 2) - (width / 2),
-    top = (screen.height / 2) - (height / 2);
-    var url = 'https://accounts.spotify.com/authorize?client_id=c00c658f3a8a4a29961ae9fe70ffde7e&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123';
-     window.open(url,
-                            'Spotify',
-                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                           );
-
 }
