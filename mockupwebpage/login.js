cedd852b70df8621b83170d07076f7a93747113a
RHowe22 <robjhowe22@gmail.com>
2019-10-06 22:55:22 -0400

    #40 able to connectto spotify authorization page

--- mockupwebpage/login.js
+++ mockupwebpage/login.js
@@ -1,11 +1,10 @@
-
-// modified from http://jsfiddle.net/JMPerez/62wafrm7/
-(function() {
+// this link works
+// https://accounts.spotify.com/authorize?client_id=c00c658f3a8a4a29961ae9fe70ffde7e&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123
+function dothis() {
     
     function login(callback) {
-        // robs client ID
         var CLIENT_ID = 'c00c658f3a8a4a29961ae9fe70ffde7e';
-        var REDIRECT_URI = 'login.html';
+        var REDIRECT_URI = 'http://localhost:8888/callback';
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
@@ -48,7 +47,7 @@
     var templateSource = document.getElementById('result-template').innerHTML,
         template = Handlebars.compile(templateSource),
         resultsPlaceholder = document.getElementById('result'),
-        loginButton = document.getElementById('btn-login');
+        loginButton = document.getElementById('btn');
     
     loginButton.addEventListener('click', function() {
         login(function(accessToken) {
@@ -59,3 +58,25 @@
                 });
             });
     });
+    
+}
+function test(){
+    window.addEventListener("message", function(event) {
+        var hash = JSON.parse(event.data);
+        if (hash.type == 'access_token') {
+            callback(hash.access_token);
+            document.getElementById("trythis").innerHTML=hash.accessToken;
+        }
+    }, false);
+
+    var width = 450,
+    height = 730,
+    left = (screen.width / 2) - (width / 2),
+    top = (screen.height / 2) - (height / 2);
+    var url = 'https://accounts.spotify.com/authorize?client_id=c00c658f3a8a4a29961ae9fe70ffde7e&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123';
+     window.open(url,
+                            'Spotify',
+                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                           );
+
+}
