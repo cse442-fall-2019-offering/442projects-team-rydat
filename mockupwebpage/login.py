1a4ee3e12ce653bb8a4b1437448cf3a6afa21905
RHowe22 <robjhowe22@gmail.com>
2019-10-19 20:45:06 -0400

    #40 seperated test from login.py

--- mockupwebpage/login.py
+++ mockupwebpage/login.py
@@ -12,7 +12,9 @@ import webbrowser
 class NoTokenError(Exception):
     """ there exists no token currently cached on this account"""
     pass
+
 class Login:
+    
     def __init__(self):
         ''' sets redirect page to the home page'''
         self.REDIRECT_URI = 'https://www-student.cse.buffalo.edu/CSE442-542/2019-Fall/cse-442j/'
@@ -21,6 +23,7 @@ class Login:
         self.Secret='e3fbfba45325459cb7b3b408f41f3a12'
         self.scope='user-read-email user-top-read playlist-read-private user-library-read user-read-playback-state user-read-currently-playing streaming user-read-private user-modify-playback-state'
         self.authentication=(OAuth(self.clientId,self.Secret,self.REDIRECT_URI,state=None,scope=self.scope))
+    
     ''' checks to see if there is already a token in the system if not
         check the  if there is one URL to see if there is one in the url'''
     def getToken(self,url=None):
@@ -44,53 +47,9 @@ class Login:
         if(loggedOut is True):
             dialogue="&show_dialog=true"
         webbrowser.open(self.authentication.get_authorize_url()+dialogue)
+    
+    ''' function will redirect to the authorization page and give option
+        to login in as a new user'''
     def logout(self, token):
         token=None
         self.login(True)                
-if __name__== "__main__":
-    loggingIn=Login()
-    try:        
-        token=loggingIn.getToken(loggingIn.REDIRECT_URI)
-    except NoTokenError:
-        loggingIn.login()
-        print("please enter the url in the redirected page")
-        url=input()    
-        token=loggingIn.getToken(url)
-        
-
-
-    print("\n"+str(token))
-    
-    ''' for testing purpose the tester will have to manually input the redirect uri
-        but in the application should be handled by framework '''
-    
-    print("\n here is the user information:\n")
-    sp=spotipy.client.Spotify(auth=token['access_token'])
-    print(str(sp.current_user()))
-
-
-    print("\n here are the user's top artists:\n")
-    results=sp.current_user_top_artists()
-    artistList=[]
-    for item in results['items']:
-        artistList.append(str(item['name']))
-    print(str(artistList))
-    print("\n here are the user's top tracks:\n")
-    results=sp.current_user_top_tracks()
-    for item in results['items']:
-        songList=[]
-        for artist in item['artists']:
-            songList.append(artist['name'])
-        print(str(item['name'])+" from: "+str(item['album']['name'])+" by "+str(songList))
-        
-
-    print("\n\n testing that logging out provides a new token")
-    print("\noriginal token:\n"+str(token['access_token'])+"\n")
-    print("\n press enter to logout")
-    input()
-    loggingIn.logout(token)
-    print("please enter the url in the redirected page")
-    url=input()    
-    token=loggingIn.getToken(url)
-    print("\nnewtoken:\n"+str(token['access_token'])+"\n")
-    
