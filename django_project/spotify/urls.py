e54b8f917f768606482dbaa8a9bde8c259cb1d2d
alanpinkhasik <alanpink@buffalo.edu>
2019-11-18 12:33:07 -0500

    #36 #60 Branch for webserver story and task
    Most up to date backend code

--- django_project/spotify/urls.py
+++ django_project/spotify/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='mooduse-login'), # our login is created in views.py where login is defined
-    path('home/', views.home, name='mooduse-home'),
-    path('close/', views.loginbutton,name='login-button'),
-    path('playlist-generated/', views.generated, name='mooduse-generated'),
-    path('logged-out/', views.logoutbutton, name='logout-button'),
-    path('again/', views.login, name='newplaylist-button'),
+        path('', views.login, name='mooduse-login'),
+        path('proceed/', views.loginbutton, name='login-button'),
+        path('home/', views.home, name='mooduse-home'),
+        path('playlist-generated/', views.generated, name='mooduse-generated'),
+        path('logged-out/', views.logoutbutton, name='logout-button'),
+        path('again/', views.login, name='newplaylist-button'),
 ]
