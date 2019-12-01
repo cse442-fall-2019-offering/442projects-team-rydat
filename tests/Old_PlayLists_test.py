f4ea7837bfddf1fab87fac7ef81c1bbecc3c00da
RHowe22 <robjhowe22@gmail.com>
2019-12-01 13:28:16 -0500

    #55 updated test and #51 updated playlist names to work with #52

--- tests/Old_PlayLists_test.py
+++ tests/Old_PlayLists_test.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import playlist
+from playlist import Mooduseplaylists
 from login import Login
 
 l = Login()
@@ -9,12 +9,13 @@ print("You will first need  to get an Oauth token from Spotify")
 print("press enter to open the redirect page")
 l.login()
 print("copy and paste entire url from the redirect for the token:")
-token= l.getToken(input())
+token= l.getToken(input())['access_token']
+print("valid token\n\n")
 playlists=Mooduseplaylists(token)
 
 print("press enter to see the playlists")
 input()
 for item in playlists:
-    print(item)
+    print(item[0])
 
 print("\nverify that theese are the Mooduse Generated playlists")
